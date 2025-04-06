0740c69a5476f61afac9726d6970a960d8e4f979
Ravibbk <rk9412839@gmail.com>
2025-04-06 14:50:52 +0530

    variable
    add main

---
+++ basic/variable.js
@@ -0,0 +1,18 @@
+const id=12345;
+let acname="xyz";
+var Email="xyz@gmail.com";
+//without any variable 
+ cityname="lko";
+console.log(id);
+console.log(acname);
+console.log(Email);
+console.log(cityname);
+// //change
+ //id=34; not allow in changes.
+ acname="pk";
+ Email="pk@gmail.com";
+ cityname="mi";
+ console.log(id);
+ console.log(acname);
+ console.log(Email);
+ console.log(cityname);

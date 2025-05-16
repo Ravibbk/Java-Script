cbf72408fcb747133a353a3878f17e34b9bea655
Ravibbk <rk9412839@gmail.com>
2025-05-16 12:34:51 +0530

    conversion of datatype

---
+++ basic/datatypeconversion.js
@@ -0,0 +1,12 @@
+let score = 33;
+
+
+console.log (typeof(score));
+
+let scoreInString = String(score);
+
+console.log (typeof(scoreInString));
+
+
+// if given "33abc" then output console.log (typeof(scoreInString)); is string 
+// but console.log (scoreInString); then output will NaN "not a number"

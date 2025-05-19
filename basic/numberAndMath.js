1d57d81e77b191b0ff2d590cbabbbea864ad8fa8
Ravibbk <rk9412839@gmail.com>
2025-05-19 21:57:41 +0530

    math and number

---
+++ basic/numberAndMath.js
@@ -0,0 +1,15 @@
+console.log(Math.random()); //0.1 to 0.9 ke beech 
+console.log((Math.random()*10)+1); //1 to 10 ke beech
+
+
+
+
+/*   math.floor(4.9) : gives 4
+
+  math.ceil(4.9) : gives 5 */
+
+const min = 10 
+const max = 20;
+
+console.log(Math.random() * (max-min+1) +min);// 10 to 20 ke beech.
+ //for proper integer we use the floor or ceil.

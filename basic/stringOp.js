d3a466634ce9d3488b7e7230a1ea71db00ed9abe
Ravibbk <rk9412839@gmail.com>
2025-05-19 15:49:20 +0530

    string

---
+++ basic/stringOp.js
@@ -0,0 +1,24 @@
+let str="gduhffyfbjdggjdnh";
+//console.log(str);
+
+//console.log(str.length);
+
+// console.log(str.toLocaleLowerCase());
+
+
+// console.log(str.toLocaleUpperCase());
+
+//console.log(str.charAt(6));
+
+// console.log(str.slice(3,9));
+
+
+// console.log(str.substring(4 ,6));
+
+
+// console.log(str.split("",4));
+ let name ="raj";
+ let branch ="cse";
+
+
+console.log(`hello my name is ${name} and my course is ${branch}`);
